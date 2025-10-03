e27ab401b14dfdccdd0567309e0ce56c2e96da16
yunjiao20 <users52013149420@outlook.com>
2025-10-03 12:40:43 +0800

    Update make.cmd

--- src/make.cmd
+++ src/make.cmd
@@ -3,3 +3,6 @@ move SRC\dist\SRC.exe SRC\
 
 pyinstaller -F -n SRC-GUI SRC-GUI\gui.py
 move SRC\dist\SRC-GUI.exe SRC-GUI\
+
+pyinstaller -F -n SRCGUIlit SRC-GUIlit\gui.py
+move SRC-GUIlit\dist\SRCGUIlit.exe SRC-GUIlit\
