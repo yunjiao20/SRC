d91a435de98bb4bb14dcfd454281853ab6b16987
yunjiao20 <users52013149420@outlook.com>
2025-10-04 21:37:18 +0800

    Delete src directory

--- src/make.cmd
+++
@@ -1,8 +0,0 @@
-pyinstaller -F -n SRC SRC\random_choice.py
-move SRC\dist\SRC.exe SRC\
-
-pyinstaller -F -n SRC-GUI SRC-GUI\gui.py
-move SRC\dist\SRC-GUI.exe SRC-GUI\
-
-pyinstaller -F -n SRCGUIlit SRC-GUIlit\gui.py
-move SRC-GUIlit\dist\SRCGUIlit.exe SRC-GUIlit\
